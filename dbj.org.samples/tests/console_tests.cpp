7ed26e1aa52a01556597d8e2f8d72949b942e940
dbjdbj <dbj@dbj.org>
2018-07-22 00:37:55 +0200

    advancing

--- dbj.org.samples/tests/console_tests.cpp
+++ dbj.org.samples/tests/console_tests.cpp
@@ -3,6 +3,10 @@
 
 #include "../stdafx.h"
 
+static char space[]{ " " };
+static char line[]{ "------------------------------------------------------------" };
+
+
 template< typename T> 
 struct dbj_type final 
 {
@@ -24,8 +28,11 @@ struct dbj_type final
 	static std::string to_string( void ) noexcept
 	{
 		return 
+		std::string{ "\ndefault type" } + 
 		std::string { dbj_type::descriptor_to_string<dbj_type::def_type>( ) } +
+		std::string{ "\nactual type" } +
 		std::string { dbj_type::descriptor_to_string<dbj_type::actual_type>() } +
+		std::string{ "\nunderlying type" } +
 		std::string { dbj_type::descriptor_to_string<dbj_type::under_type>() };
 	}
 
@@ -33,12 +40,14 @@ struct dbj_type final
 	static std::string descriptor_to_string() noexcept
 	{
 		return DBJ::printf_to_buffer(
-			"\n%-20s : %s, %s\n%-20s %zu \n%-20s %zu",
+			"\n%-20s" 
+			"\n%-20s : %s / %s"
+			"\n%-20s : %s -- %zu"
+			"\n%-20s : %s -- %zu",
 			DBJ_TYPENAME(descriptor::type),
-			(descriptor::is_pointer ? "Pointer" : "NOT Pointer"),
-			(descriptor::is_array ? "Array" : "NOT Array"),
-			"dimensions if array", descriptor::number_of_dimension,
-			"length if array", descriptor::first_extent
+			space, (descriptor::is_pointer ? "Pointer" : "NOT Pointer"), (descriptor::is_array ? "Array" : "NOT Array"),
+			space, "dimensions if array", descriptor::number_of_dimension,
+			space, "dimension[0] size if array", descriptor::first_extent
 		);
 	}
 };
@@ -49,18 +58,20 @@ struct dbj_type final
 template<typename T>
 auto  dbj_type_report = []( dbj::console::IConsole * CON = nullptr )
 { 
-
-		static char space[]{ " " };
-		static char line[]{ "------------------------------------------------------------" };
-		printf_to_console(CON, "\n%s", T::to_string<T>() );
+		printf_to_console(CON, "\n%s", T::to_string<T>().c_str() );
 };
 
+/// <summary>
+/// out overloads for outputing to IConsole instance
+/// </summary>
 namespace dbj::console {
 	/*	the one and only, only the lonely ...etc ...	*/
 template <typename T>
 void out(T specimen)
 { 
 	using argument_type = dbj_type<T> ;
+	printf_to_console(&console_, "\n%s", line);
+	DBJ_TYPE_REPORT_FUNCSIG(&console_);
 	dbj_type_report<argument_type>( &console_ );
 }
 
@@ -70,20 +81,21 @@ void out(T specimen)
 	void out(T(&native_array)[N])
 	{
 		using argument_type = dbj_type<decltype(native_array)>;
-		printf_to_console(&console_, "\n");
+		printf_to_console(&console_, "\n%s", line);
+		DBJ_TYPE_REPORT_FUNCSIG(&console_);
 		dbj_type_report<argument_type>( &console_);
 		printf_to_console(&console_, "\n");
 
 		if constexpr ( std::is_same_v<std::remove_cv_t<T>, char > ) {
-			printf_to_console(&console_, "%s", native_array);
+			printf_to_console(&console_, "%-20s : %s", space, native_array);
 		}
 		else
 		if constexpr (std::is_same_v<std::remove_cv_t<T>, wchar_t >) {
-			printf_to_console(&console_, L"%s", native_array);
+			printf_to_console(&console_, L"%-20S : %s", space, native_array);
 		}
 		else
 		if constexpr (std::is_integral_v<T>) {
-			printf_to_console(&console_, " {");
+			printf_to_console(&console_, "%-20s : {", space );
 			for (auto && elem : native_array) {
 				printf_to_console(&console_, " %d ", elem);
 			}
@@ -94,7 +106,7 @@ void out(T specimen)
 	template <typename T, size_t N>
 	void out( T(*native_array)[N])
 	{
-		printf_to_console(&console_, "\n");
+		printf_to_console(&console_, "\n%s", line);
 		DBJ_TYPE_REPORT_FUNCSIG(&console_);
 		out<T,N>(*native_array);
 	}
@@ -106,7 +118,7 @@ void out(T specimen)
 template<typename T>
 struct actual final {
 	using type = std::remove_cv_t< std::remove_pointer_t< T > >;
-	constexpr inline static const type dflt{};
+	constexpr inline static const type value{};
 };
 
 // DBJ_TEST_UNIT("console")
@@ -116,13 +128,16 @@ void dbj_test_console()
 
 	using A9 = dbj::arr::ARH<int, 9>;
 
+	// native arrays out
 	out(A9::ARR{ { 1,2,3,4,5,6,7,8,9 } });
 	out(A9::to_arp(A9::ARR{ { 1,2,3,4,5,6,7,8,9 } }));
 	out(A9::to_arf(A9::ARR{ { 1,2,3,4,5,6,7,8,9 } }));
 
-
-	const wchar_t (&& buf)[]{ L"ABCDEFGHIJKLMNOPQR" };
+	// pointers out
 	out("R/O string literal");
+	out(std::addressof(dbj::console::out<int>));
+
+	const wchar_t(&& buf)[] { L"ABCDEFGHIJKLMNOPQR" };
 	out(buf);
 	out(buf[0]);
 	out(&buf[0]);
